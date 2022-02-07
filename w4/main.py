9c9c28cbc99b6694d088daef74831fcc18264d1b
Jordan <jordanadams151@gmail.com>
2022-02-06 21:24:06 -0800

    readme added

--- w4/main.py
+++ w4/main.py
@@ -1,34 +1,34 @@
 import random
 
 # function to choose dress type
-def choose_wear():
-    print("Select Dress Type: ")
-    print("1.Casual")
-    print("2.Party Wear")
+# def choose_wear():
+# # print("Select Dress Type: ")
+# print("1.Casual")
+# print("2.Party Wear")
 
 
 # getting user input until its a valid one
-    while True:
-        # getting user input
-        choice = input("Enter choice(1/2): ")
-        if choice in ('1', '2'):
-            if choice == '1':
-                dress = "Casual"
-                return dress
-            else:
-                dress = "Party Wear"
-                return dress
-            break
-        else:
-            print("Invalid Input")
-            print("Select Season: ")
-            print("1.Spring")
-            print("2.Summer")
-            print("3.Fall")
-            print("4.Winter")
+# while True:
+# getting user input
+# choice = input("Enter choice(1/2): ")
+# if choice in ('1', '2'):
+# if choice == '1':
+# dress = "Casual"
+# return dress
+# else:
+# dress = "Party Wear"
+# return dress
+# break
+# else:
+# print("Invalid Input")
+# print("Select Season: ")
+# print("1.Spring")
+# print("2.Summer")
+# print("3.Fall")
+# print("4.Winter")
 
 # randome number generator
-random = random.randrange(1, 2)
+# random = random.randrange(1, 2)
 
 # loop
 while True:
@@ -40,15 +40,15 @@ while True:
     if choice in choice_list:
         if choice == '1':
             season = "Spring"
-            dress = choose_wear()
-            if dress == "Casual":
-                if random == 1:
-                    # f string
-                    print(f"For {season} Try Shorts with T-shirt")
-                elif random == 2:
-                    print(f"For {season} Try Trousers with pack shirt")
-            elif dress == "Party Wear":
-                if random == 1:
-                    print(f"For {season} Try Patterned full sleeves")
-                elif random == 2:
-                    print(f"For {season} Try Patterned short sleeves")
+# dress = choose_wear()
+# if dress == "Casual":
+#               if random == 1:
+#                    # f string
+#                    print(f"For {season} Try Shorts with T-shirt")
+#                elif random == 2:
+#                    print(f"For {season} Try Trousers with pack shirt")
+#            elif dress == "Party Wear":
+#                if random == 1:
+#                    print(f"For {season} Try Patterned full sleeves")
+#                elif random == 2:
+#                    print(f"For {season} Try Patterned short sleeves")
---
+++ w4/readme.md
@@ -0,0 +1,4 @@
+# For this learn together, I wanted to code a decision on what you would wear depending 
+# on what the weather was. To be honest, I had a lot of trouble trying to figure out how to write my code
+# so I rewatched the videos over and over but still couldnt get it.
+# I will be reaching out to get some 1 on 1
